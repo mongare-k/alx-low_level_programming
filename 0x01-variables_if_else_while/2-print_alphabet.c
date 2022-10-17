bf1db6344f6cd7a11153e8e02f547740b626f109
Kevin Mong'are <molessk@gmail.com>
2022-10-17 04:46:45 +0000

    Alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+	        putchar('\n');
+
+	return(0);
+}
