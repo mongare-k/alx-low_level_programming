b91dea191043d6ed9ef82de17a2e6eb0d8eabfca
Kevin Mong'are <molessk@gmail.com>
2022-10-17 20:00:27 +0000

    All possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints all possible combinations of two digits,
+ * numbers must be separated by a , followed by a space,
+ * the two digits must be different
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int m, n;
+
+	for (m = '0'; m < '9'; m++)
+	{
+		for (n = m + 1; n <= '9'; n++)
+		{
+			if (n != m)
+			{
+				putchar(m);
+				putchar(n);
+
+				if (n == '8' && m == '9')
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
