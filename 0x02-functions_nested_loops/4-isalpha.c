6d21bdae33e65a05fcabfa3864cf5e72f26b4c94
Kevin Mong'are <molessk@gmail.com>
2022-10-19 22:06:18 +0000

    Alphabetic characters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+int_isalpha(int c)
+{
+	char lower, upper;
+	int isletter = 0;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		for (upper = 'A'; upper <= 'Z'; upper++)
+		{
+			if (c == lower || c == upper)
+				isletter = 1;
+		}
+	}
+	return (isletter)
+}
