e241f3eb2dc25152b55e56c88b7311600d080a35
Kevin Mong'are <molessk@gmail.com>
2022-10-20 18:19:37 +0000

    Executables

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+
+/*
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2,
+ * followed by a new line
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+
