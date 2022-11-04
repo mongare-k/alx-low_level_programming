ff2d948fe8b5907c60ad331147432e201fcffcae
Kevin Mong'are <molessk@gmail.com>
2022-11-04 23:19:32 +0000

    Square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -25,13 +25,9 @@ int _sqrt_recursion(int n)
  */
 int _sqrt(int n, int i)
 {
-	int sqrt = i * i;
-
-	if (sqrt > n)
+	if (n > i)
 		return (-1);
-
-	if (sqrt == n)
-		return (i);
-
-	return (_sqrt(n, i + 1));
+	else if (n * n == i)
+		return (n);
+	return (_sqrt(n + 1, i));
 }
